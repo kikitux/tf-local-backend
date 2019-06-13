cd4ebe77d825d29aa06f6dc17337e31f17115c0a
chavo1 <chavdar.rakov@gmail.com>
2019-06-13 10:36:01 +0300

    test

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+resource "tfe_workspace" "test" {
+  name                  = "tf-local-backend"
+  organization          = "chavo4"
+  working_directory     = "test"
+  terraform_version     = "0.11.14"
+  file_triggers_enabled = false
+
+}
+
 resource "null_resource" "helloWorld" {
   provisioner "local-exec" {
     command = "echo hello world"
